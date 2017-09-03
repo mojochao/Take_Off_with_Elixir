ebee52d40ad1933207f922b6a6757117cf16bcf5
Allen Gooch <allen.gooch@gmail.com>
2017-09-03 15:42:20 -0500

    Add initial Rocketry impl.

--- redfour/physics/test/physics_test.exs
+++ redfour/physics/test/physics_test.exs
@@ -2,7 +2,15 @@ defmodule PhysicsTest do
   use ExUnit.Case
   doctest Physics
 
-  test "greets the world" do
-    assert Physics.hello() == :world
+  test "escape velocity of earth is correct" do
+    ev = Physics.Rocketry.escape_velocity(:earth) |> IO.inspect
+    assert ev == 11.2 #what should this value be?
   end
+
+  test "escape velocity of planet X is correct" do
+    planet_x = %{mass: 4.0e22, radius: 6.21e6}
+    ev =  planet_x |> Physics.Rocketry.escape_velocity |> IO.inspect
+    assert ev == 1.0 #what should this value be?
+  end
+
 end
